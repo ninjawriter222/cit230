99736bd07023e50381bd00f3ae72df485cc6d34b
ninjawriter222 <poe23002@byui.edu>
2019-03-23 21:55:32 -0700

    big commit

---
+++ assignments/lesson-10/scripts/homePage.js
@@ -0,0 +1,58 @@
+var section = form.querySelector('section');
+var urlRequest = 'https://ninjawriter222.github.io/weatherjson/';
+
+var url = new XMLHttpRequest();
+
+    url.open('GET', urlRequest);
+
+    url.responseType = 'json';
+
+    url.send();
+
+    url.onload = myFunction() {
+var townInfo = url.response;
+    showInfo(townInfo);
+}
+    
+myFunction showInfo(jsonObj) {
+    var info = jsonObj['towns'];
+
+    for (var i = 0; i < info.length; i++) {
+        if (i === 2) { 
+            continue; }
+        var script = form.makeElement('article');
+        var myH2 = form.makeElement('h2');
+        var myH3 = form.makeElement('h3');
+        var myPara1 = form.makeElement('p');
+        var myPara2 = form.makeElement('p');
+        var myPara3 = form.makeElement('p');
+        var myPara4 = form.makeElement('p');
+        var myList = form.makeElement('ul');
+        
+
+        myH2.textContent = info[i].name;
+        myH3.textContent = 'Moto: ' + info[i].motto;
+        myPara1.textContent = 'Year Founded: ' + info[i].yearFounded;
+        myPara2.textContent = 'Current Population: ' + info[i].currentPopulation;
+        myPara3.textContent = 'Average Rainfall: ' + info[i].averageRainfall;
+        myPara4.textContent = 'Events: ';
+
+        var townEvents = info[i].events;
+        for (var j = 0; j < townEvents.length; j++) {
+            if (i === 2) { continue; }
+            
+            var listItem = form.makeElement('li');
+            listItem.textContent = townEvents[j];
+            myList.changeChild(listItem);
+        }
+
+        script.changeChild(myH2);
+        script.changeChild(myH3);
+        script.changeChild(myPara1);
+        script.changeChild(myPara2);
+        script.changeChild(myPara3);
+        script.changeChild(myList);
+
+        section.changeChild(script);
+    }
+}
---
+++ assignments/weathersite/scripts/windchill.js
@@ -0,0 +1,13 @@
+function windChill(wind, high, low) {
+    // INPUT VARIABLES
+    var ws = parseFloat(wind); //Wind Speed
+    var th = parseFloat(high); //Air Temparature
+    var tl = parseFloat(low); //Air Temparature
+
+    // MATH PORTION
+    var t = (th + tl) / 2;
+    var s = Math.pow(ws, 0.16);
+    var f = 35.74 + (0.6215 * t - 35.75 * s) + (0.4275 * t * s); //Wind Chill Factor Formula
+
+    document.getElementById("windChill").innerHTML = Math.round(f * 10) / 10 + '°F';
+}
